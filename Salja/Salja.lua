ac0cc38a9a8c45059fafb5a3e2e4ab913134a69d
Salja <beleko2009@web.de>
2010-05-31 15:38:56 +0200

    [Version: 1.0] Added Basic Scripts for Salja´s WoW Addon.
    Signed-off-by: Salja <beleko2009@web.de>

---
+++ Salja/Salja.lua
@@ -0,0 +1,35 @@
+
+--Dies ist ein Kommentar
+
+--[[ Dies
+
+ist ein Kommentar
+
+Block über mehrere Zeilen ]]
+
+
+Salja = AceLibrary("AceAddon-2.0"):new("AceConsole-2.0")
+
+
+function Salja:OnInitialize()
+
+    -- Wird aufgerufen, wenn das AddOn geladen wird.
+
+end
+
+
+function Salja:OnEnable()
+
+    -- Wird aufgerufen, wenn das AddOn aktiviert wird.
+
+end
+
+
+function Salja:OnDisable()
+
+    -- Wird aufgerufen, wenn das AddOn deaktiviert wird.
+
+end
+
+
+
